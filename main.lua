b8de669fb241d729589c0cdf001a7c9de808a442
walter <walter@coronalabs.com>
2014-04-29 17:35:46 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
 local viewer = require "plugin.viewer"
 
 viewer.run()
+
+-- Display message if run in simulator
+if ( "simulator" == system.getInfo("environment") ) then
+	local text = "To use CoronaViewer, build this project and install it on your iOS device."
+	local message = display.newText{
+		text = text,
+		x = display.contentCenterX,
+		y = display.contentCenterY,
+		width = display.contentWidth - 100,
+		fontSize = 16,
+		align = "center",
+	}
+end
